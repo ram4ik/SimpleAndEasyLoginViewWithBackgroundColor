86042e4acb15ee51d0bfc5b16161cf1cf7befb9b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-28 15:59:37 +0200

    init

--- SimpleAndEasyLoginViewWithBackgroundColor/ContentView.swift
+++ SimpleAndEasyLoginViewWithBackgroundColor/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple, Color.red]), startPoint: .bottomLeading, endPoint: .topTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading, spacing: 14) {
+                
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 100, height: 100)
+                    .overlay(Circle().stroke(Color.black, lineWidth: 2))
+                    .offset(x: 135)
+                    .opacity(0.2)
+                
+                Group {
+                    Text("Email").foregroundColor(.gray)
+                    TextField("Email...", text: self.$email).padding()
+                        .background(Color(red: 234.0/255.0, green: 233.0/255.0, blue: 235.0/255.0, opacity: 0.5))
+                        .cornerRadius(12)
+                    }
+                
+                Group {
+                    Text("Password").foregroundColor(.gray)
+                    TextField("Password...", text: self.$password).padding()
+                        .background(Color(red: 234.0/255.0, green: 233.0/255.0, blue: 235.0/255.0, opacity: 0.5))
+                        .cornerRadius(12)
+                    }
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Login").padding(16)
+                        .foregroundColor(.white)
+                }.background(Color(red: 234.0/255.0, green: 233.0/255.0, blue: 235.0/255.0, opacity: 0.5))
+                    .cornerRadius(10)
+                    .offset(x: 150)
+            }.padding()
+            
+        }
     }
 }
 
